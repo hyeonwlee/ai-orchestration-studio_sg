ad80acb6c25bfc30f1a2c7299117034632301fb9
hwlee <hyeonweel@gmail.com>
2026-09-10 14:31:34 +0900

    feat: add class 'Order'

--- shop.py
+++ shop.py
@@ -13,3 +13,19 @@ class Customer:
 
     def summary(self):
         return f"[{self.grade}] {self.name} (포인트: {self.points:,})"
+
+class Order:
+    def __init(self, order_id, customer, items):
+        self.order_id = order_id
+        self.customer = customer
+        self.items = items
+
+    def total_price(self):
+        sum_of_items = sum([item[1] for item in self.items])
+        return int(sum_of_items * (1 - self.get_discount_rate()))
+
+    def add_item(self, name, price):
+        self.items += (name, price)
+
+    def pay(self):
+        self.customer.add_points(self.total_price())
